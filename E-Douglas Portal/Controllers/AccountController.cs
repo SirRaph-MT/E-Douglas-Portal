8cac307e3643b5048ff556d25ea277e05ea86307
SirRaph <raphaelezeomas@gmail.com>
2026-02-17 12:42:51 +0100

    Login view and styling

---
+++ E-Douglas Portal/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Douglas_Portal.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
